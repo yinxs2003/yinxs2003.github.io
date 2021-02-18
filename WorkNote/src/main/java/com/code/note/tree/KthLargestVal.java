ce640e834d91beaf789820c989c9c50d7ca48b4e
yinxuesong <yinxuesong@yidian-inc.com>
2021-02-18 10:06:43 +0800

    Update 二叉搜索树的第k大节点值

--- WorkNote/src/main/java/com/code/note/tree/KthLargestVal.java
+++ WorkNote/src/main/java/com/code/note/tree/KthLargestVal.java
@@ -5,6 +5,39 @@ import java.util.List;
 
 public class KthLargestVal {
     private List<Integer> kthLargestValList = new ArrayList<>();
+    int ans, cnt;
+
+    public int kthLargest2(TreeNode root, int k) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        kthLargestNode2(root, k);
+        Integer a = kthLargestValList.get(k - 1);
+        System.out.println(a);
+        return a;
+    }
+
+    /**
+     * 二叉搜索树特点：中序遍历是有序的
+     *
+     * @param x
+     */
+
+    public void kthLargestNode2(TreeNode x, int k) {
+        if (x == null) {
+            return;
+        }
+
+        kthLargestNode2(x.right, k);
+        kthLargestValList.add((Integer) x.val);
+        if (kthLargestValList.size() == k) {
+            return;
+        }
+        kthLargestNode2(x.left, k);
+    }
+
 
     public int kthLargest(TreeNode root, int k) {
 
@@ -12,29 +45,25 @@ public class KthLargestVal {
             return 0;
         }
 
-        kthLargestNode(root);
-        Integer a = kthLargestValList.get(kthLargestValList.size() - k);
-        System.out.println(a);
-        return a;
+        kthLargestNode(root, k);
+        System.out.println(ans);
+        return ans;
     }
 
-    /**
-     *
-     * 二叉搜索树特点：中序遍历是有序的
-     *
-     * @param x
-     */
 
-    public void kthLargestNode(TreeNode x) {
+    public void kthLargestNode(TreeNode x, int k) {
         if (x == null) {
             return;
         }
 
-        kthLargestNode(x.left);
-        kthLargestValList.add((Integer) x.val);
-        kthLargestNode(x.right);
-    }
+        kthLargestNode(x.right, k);
+        if (++cnt == k) {
+            ans = (int) x.val;
+            return;
+        }
+        kthLargestNode(x.left, k);
 
+    }
 
     public static void main(String[] args) {
         OrderTree t = new OrderTree();
@@ -50,5 +79,8 @@ public class KthLargestVal {
 
         KthLargestVal node = new KthLargestVal();
         node.kthLargest(root, 1);
+
+        node.kthLargest2(root, 1);
+
     }
 }
