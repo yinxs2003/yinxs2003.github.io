516de6bb055c86af72f36059188aef77c3bce457
yinxuesong <yinxuesong@yidian-inc.com>
2021-04-15 10:50:56 +0800

    分离文档

--- WorkNote/src/main/java/com/code/note/annotation/Main.java
+++
@@ -1,7 +0,0 @@
-package com.code.note.annotation;
-
-public class Main {
-    public static void main(String[] args) {
-        EventInfoUtil.getEventInfo(EventBean.class);
-    }
-}
--- WorkNote/src/main/java/com/code/note/test/Test3.java
+++
@@ -1,7 +0,0 @@
-package com.code.note.test;
-
-public class Test3 {
-    public static void main(String[] args){
-        System.out.println(-1>>>1);
-    }
-}
